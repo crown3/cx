fdec4ef6360535601947c1f250b1026cc21f17fb
crown chen <chenjuncrown@gmail.com>
2021-05-14 15:33:27 +0800

    feat(cx): update cx interface to more flexible handle of both css module and css
    When cx without bind this, treat all args as normal css;But when cx without bind this and the first
    arg is array,the first arg is handled as css module and others treat as normal css
    
    BREAKING CHANGE: cx fn interface has changed
    
    fix #1

--- src/index.ts
+++ src/index.ts
@@ -4,54 +4,74 @@ interface ModuleStyles {
 
 type Value = string | number | boolean | undefined | null
 
-type ObjectItem = {
-  [key in string | number]: Value
-}
+type Mapping = Record<string, unknown>
 
-type Item = Value | ObjectItem | (Value | ObjectItem)[]
+type Arg = Value | Mapping | Arg[]
 
-function getClassNames(
-  this: ModuleStyles,
-  arr: Item[],
-  isModule: boolean = true
-): string {
+function getClassNames(this: ModuleStyles | undefined, arr: Arg[]): string {
+  if (!arr.length) return ''
+
   const classes: string[] = []
   const hasOwn = {}.hasOwnProperty
   for (let i = 0; i < arr.length; i++) {
     const item = arr[i]
+
     if (!item || item === true) continue
+
     if (typeof item === 'number' || typeof item === 'string') {
-      if (isModule) {
-        if (this[item]) {
-          classes.push(this[item])
-        }
+      if (this !== undefined) {
+        classes.push(this[item])
       } else {
         classes.push(item + '')
       }
     } else if (Array.isArray(item)) {
-      classes.push(getClassNames.call(this, item, isModule))
+      // ignore empty string
+      classes.push(getClassNames.call(this, item))
     } else if (typeof item === 'object') {
-      for (const key in item) {
-        if (hasOwn.call(item, key) && item[key]) {
-          classes.push(isModule ? this[key] : key)
+      // check if obj has custom toString method
+      if (item.toString === Object.prototype.toString) {
+        for (const key in item) {
+          if (hasOwn.call(item, key) && item[key]) {
+            classes.push(this !== undefined ? this[key] : key)
+          }
         }
+      } else {
+        classes.push(
+          this !== undefined ? this[item.toString()] : item.toString()
+        )
       }
     }
   }
-  return classes.join(' ')
+  // filter empty string
+  return classes.reduce((acc, cur) => {
+    if (!cur) return acc
+    if (acc) {
+      return acc + ' ' + cur
+    } else {
+      return cur
+    }
+  }, '')
 }
 
-export function classNames(
-  this: ModuleStyles,
-  module: Item[],
-  styles?: Item[]
-): string {
-  let res = getClassNames.call(this, module)
-  if (styles) {
-    if (res) res += ' '
-    res += getClassNames.call(this, styles, false)
+type ThisType = ModuleStyles | typeof globalThis | undefined
+
+function isModuleStyles(_this: ThisType): _this is ModuleStyles {
+  return typeof _this === 'object' && _this !== globalThis
+}
+
+export default function classNames(this: ThisType, ...args: Arg[]): string {
+  if (isModuleStyles(this) && Array.isArray(args[0])) {
+    const firstArg = args.shift()
+    let moduleClasses = getClassNames.call(this, [firstArg])
+    let classes = getClassNames.call(undefined, args)
+    if (moduleClasses && classes) {
+      return moduleClasses + ' ' + classes
+    } else {
+      return moduleClasses ? moduleClasses : classes
+    }
+  } else {
+    return getClassNames.call(undefined, args)
   }
-  return res
 }
 
 export type CX = OmitThisParameter<typeof classNames>
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,38 +1,189 @@
-import { classNames, CX } from '../src'
+import classNames, { CX } from '../src'
 
-describe('cx function test', () => {
+describe('cx without binding this', () => {
+  let cx: CX = classNames
+  it('keeps object keys with truthy values', () => {
+    expect(
+      cx({
+        a: true,
+        b: false,
+        c: 0,
+        d: null,
+        e: undefined,
+        f: 1,
+      })
+    ).toBe('a f')
+  })
+
+  it('joins arrays of class names and ignore falsy values', () => {
+    expect(cx('a', 0, null, undefined, true, 1, 'b')).toBe('a 1 b')
+  })
+
+  it('supports heterogenous arguments', () => {
+    expect(cx({ a: true }, 'b', 0)).toBe('a b')
+  })
+
+  it('should be trimmed', () => {
+    expect(cx('', 'b', {}, '')).toBe('b')
+  })
+
+  it('returns an empty string for an empty configuration', () => {
+    expect(cx({})).toBe('')
+  })
+
+  it('supports an array of class names', function() {
+    expect(cx(['a', 'b'])).toBe('a b')
+  })
+
+  it('joins array arguments with string arguments', () => {
+    expect(cx(['a', 'b'], 'c')).toBe('a b c')
+    expect(cx('c', ['a', 'b'])).toBe('c a b')
+  })
+
+  it('handles multiple array arguments', () => {
+    expect(cx(['a', 'b'], ['c', 'd'])).toBe('a b c d')
+  })
+
+  it('handles arrays that include falsy and true values', () => {
+    expect(cx(['a', 0, null, undefined, false, true, 'b'])).toBe('a b')
+  })
+
+  it('handles arrays that include arrays', () => {
+    expect(cx(['a', ['b', 'c']])).toBe('a b c')
+  })
+
+  it('handles arrays that include objects', () => {
+    expect(cx(['a', { b: true, c: false }])).toBe('a b')
+  })
+
+  it('handles deep array recursion', () => {
+    expect(cx(['a', ['b', ['c', { d: true }]]])).toBe('a b c d')
+  })
+
+  it('handles arrays that are empty', () => {
+    expect(cx('a', [])).toBe('a')
+  })
+
+  it('handles nested arrays that have empty nested arrays', () => {
+    expect(cx('a', [[]])).toBe('a')
+  })
+
+  it('handles all types of truthy and falsy property values as expected', () => {
+    expect(
+      cx({
+        // falsy:
+        null: null,
+        emptyString: '',
+        noNumber: NaN,
+        zero: 0,
+        negativeZero: -0,
+        false: false,
+        undefined: undefined,
+
+        // truthy (literally anything else):
+        nonEmptyString: 'foobar',
+        whitespace: ' ',
+        function: Object.prototype.toString,
+        emptyObject: {},
+        nonEmptyObject: { a: 1, b: 2 },
+        emptyList: [],
+        nonEmptyList: [1, 2, 3],
+        greaterZero: 1,
+      })
+    ).toBe(
+      'nonEmptyString whitespace function emptyObject nonEmptyObject emptyList nonEmptyList greaterZero'
+    )
+  })
+
+  it('handles toString() method defined on object', () => {
+    expect(
+      cx({
+        toString() {
+          return 'classFromMethod'
+        },
+      })
+    ).toBe('classFromMethod')
+  })
+
+  it('handles toString() method defined inherited in object', () => {
+    class Class1 {
+      toString() {
+        return 'classFromMethod'
+      }
+    }
+    class Class2 extends Class1 {}
+    const obj = new Class2()
+
+    expect(cx(obj as {})).toBe('classFromMethod')
+  })
+})
+
+describe('cx with binding this', () => {
   let cx: CX
   beforeAll(() => {
     const mockStyles = {
-      foo: 'foo-1',
-      bar: 'bar-2',
+      a: 'a1',
+      b: 'b2',
     }
     cx = classNames.bind(mockStyles)
   })
 
-  it('generate correct css module className and style className', () => {
-    expect(cx(['foo', 'bar'])).toBe('foo-1 bar-2')
-    expect(cx(['foo', { bar: true }])).toBe('foo-1 bar-2')
-    expect(cx([{ foo: true }, { bar: true }])).toBe('foo-1 bar-2')
-    expect(cx([{ foo: true, bar: true }])).toBe('foo-1 bar-2')
-    expect(cx([['foo', 'bar']])).toBe('foo-1 bar-2')
-    expect(cx([['foo', { bar: null }]])).toBe('foo-1')
+  it('first arg is arr, handle this as css module', () => {
+    expect(cx(['a'], 'a')).toBe('a1 a')
+  })
 
-    expect(cx(['foo-bar'])).toBe('')
-    expect(cx([{ 'foo-bar': true }])).toBe('')
-    expect(cx([['foo-bar']])).toBe('')
+  it('first arg is not arr, handle this as normal css', () => {
+    expect(cx('a', 'b')).toBe('a b')
+  })
 
-    expect(cx([null, false, 'bar', undefined, 0])).toBe('bar-2')
-    expect(cx([null, false, 'bar', undefined, 0, 1])).toBe('bar-2')
+  it('css module will ignore falsy values and styles which not exist', () => {
+    expect(
+      cx([
+        { a: true, b: 1, c: 0, d: false, e: null, f: undefined },
+        [true, 'b', false, null, undefined],
+        true,
+        1,
+        0,
+        false,
+        null,
+        undefined,
+      ])
+    ).toBe('a1 b2 b2')
+  })
 
-    expect(cx(['foo'], ['name'])).toBe('foo-1 name')
-    expect(cx([], ['name'])).toBe('name')
-    expect(cx(['foo'], [{ name: true }])).toBe('foo-1 name')
+  it('css module handles toString() method defined on object', () => {
     expect(
       cx(
-        ['foo'],
-        ['name1', { name2: 0 }, ['name3', { name4: true }, { name5: 0 }]]
+        [
+          {
+            toString() {
+              return 'a'
+            },
+          },
+          {
+            toString() {
+              return 'c'
+            },
+          },
+        ],
+        {
+          toString() {
+            return 'b'
+          },
+        }
       )
-    ).toBe('foo-1 name1 name3 name4')
+    ).toBe('a1 b')
+  })
+
+  it('css module handles toString() method defined inherited in object', () => {
+    class Class1 {
+      toString() {
+        return 'a'
+      }
+    }
+    class Class2 extends Class1 {}
+    const obj = new Class2()
+
+    expect(cx([obj as {}], [obj as {}])).toBe('a1 a')
   })
 })
